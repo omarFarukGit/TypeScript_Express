4be9d2a23da7ad1da430d0e6a4542e5b0bdd82b2
omarFarukGit <muhammadabuhafz@gmail.com>
2026-05-14 06:08:00 +0600

    Add dotenv and pg dependencies; implement user creation endpoint

--- TS_Ex_1/src/index.ts
+++ TS_Ex_1/src/index.ts
@@ -1,12 +1,33 @@
 import express from "express";
 import type { Application, Request, Response } from "express";
+import { Pool } from "pg";
+import 'dotenv/config'
+
 const app: Application = express();
 const port = 3000;
+
 app.use(express.json());
+
+const pool = new Pool({
+  connectionString:process.env.NEON_URI,
+});
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+});
+app.post("/user", (req: Request, res: Response) => {
+  const { name, email } = req.body;
+
+  res.status(201).json({
+    message: "created user succesfully",
+    data: {
+      name,
+      email,
+    },
+  });
   console.log(req.body);
 });
 
