dbaad01d987e107d15c6ae1d11dc4829c4699bf5
omarFarukGit <muhammadabuhafz@gmail.com>
2026-05-14 08:19:57 +0600

    Enhance user update functionality and add user deletion endpoint with error handling

--- TS_Ex_1/src/index.ts
+++ TS_Ex_1/src/index.ts
@@ -118,9 +118,11 @@ app.put("/user/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
   const { name, password, age, is_active } = req.body;
   try {
+    // name=COALESCE($1, filedname) MEAN data null patale update hobe nah oi field er agr tai thakbe
     const result = await pool.query(
       `
-           UPDATE users SET name=$1,password=$2,age=$3 ,is_active=$4 WHERE id=$5 RETURNING * 
+           UPDATE users SET name=COALESCE($1, name)
+           ,password=$2,age=$3 ,is_active=$4 WHERE id=$5 RETURNING * 
             `,
       [name, password, age, is_active, id],
     );
@@ -140,6 +142,39 @@ app.put("/user/:id", async (req: Request, res: Response) => {
   } catch (error: any) {
     return res.status(201).json({
       success: true,
+      message: error.message,
+      error: error,
+    });
+  }
+});
+
+app.delete("/user/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query(
+      `
+           DELETE FROM users WHERE id=$1
+            `,
+      [id],
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "user not found",
+        data: {},
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "user deleted successfully",
+      data: {},
+    });
+  } catch (error: any) {
+    return res.json({
+      success: false,
       message: error.message,
       error: error,
     });
