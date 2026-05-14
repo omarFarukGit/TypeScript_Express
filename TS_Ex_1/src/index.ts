1c730ebfaa7271df649376e0ec60185de7feb1e7
omarFarukGit <muhammadabuhafz@gmail.com>
2026-05-14 06:59:09 +0600

    Fix user table creation and enhance user creation endpoint in index.ts

--- TS_Ex_1/src/index.ts
+++ TS_Ex_1/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import type { Application, Request, Response } from "express";
-import { Pool } from "pg";
+import { Pool, Result } from "pg";
 import "dotenv/config";
 
 const app: Application = express();
@@ -15,10 +15,10 @@ const pool = new Pool({
 const initDB = async () => {
   try {
     await pool.query(`
-        CREATE TABLE IF NOT EXISTS ueser(
+        CREATE TABLE IF NOT EXISTS users(
         id SERIAL PRIMARY KEY,
         name VARCHAR(20),
-        email VARCHAR(20) NOT NULL,
+        email VARCHAR(20) UNIQUE NOT NULL,
         password VARCHAR(20) NOT NULL,
         is_active BOOLEAN DEFAULT true,
         age INT,
@@ -39,16 +39,27 @@ app.get("/", (req: Request, res: Response) => {
 app.post("/", (req: Request, res: Response) => {
   console.log(req.body);
 });
-app.post("/user", (req: Request, res: Response) => {
-  const { name, email } = req.body;
+app.post("/user", async (req: Request, res: Response) => {
+  const { name, email, password, age } = req.body;
 
-  res.status(201).json({
-    message: "created user succesfully",
-    data: {
-      name,
-      email,
-    },
-  });
+  try {
+    const result = await pool.query(
+      `
+        INSERT INTO users(name,email,password,age) VALUES($1,$2,$3,$4) RETURNING *
+        `,
+      [name, email, password, age],
+    );
+    console.log(result);
+
+    return res.status(201).json({
+      message: "created user succesfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    console.log({ message: error.message });
+    res.send({ message: error.message, error: error });
+  }
+
   console.log(req.body);
 });
 
