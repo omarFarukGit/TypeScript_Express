8b3d22ede1354be460c94e17828c367dff7e0be4
omarFarukGit <muhammadabuhafz@gmail.com>
2026-05-14 06:30:06 +0600

    Fix formatting and enhance database initialization in index.ts

--- TS_Ex_1/src/index.ts
+++ TS_Ex_1/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import type { Application, Request, Response } from "express";
 import { Pool } from "pg";
-import 'dotenv/config'
+import "dotenv/config";
 
 const app: Application = express();
 const port = 3000;
@@ -9,8 +9,29 @@ const port = 3000;
 app.use(express.json());
 
 const pool = new Pool({
-  connectionString:process.env.NEON_URI,
+  connectionString: process.env.NEON_URI,
 });
+
+const initDB = async () => {
+  try {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS ueser(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(20),
+        email VARCHAR(20) NOT NULL,
+        password VARCHAR(20) NOT NULL,
+        is_active BOOLEAN DEFAULT true,
+        age INT,
+
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )   
+        `);
+    console.log("db connect successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
@@ -31,6 +52,7 @@ app.post("/user", (req: Request, res: Response) => {
   console.log(req.body);
 });
 
+await initDB();
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
